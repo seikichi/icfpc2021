b0eb9e3b41758090ac2ed96f32928fe24155463c
Kohei Nagira <qwerty.knagira@gmail.com>
2021-07-09 22:18:25 +0900

    implement render button

--- visualizer/index.js
+++ visualizer/index.js
@@ -1,7 +1,7 @@
-const problem = {
+let problem = {
     "hole": [[45, 80], [35, 95], [5, 95], [35, 50], [5, 5], [35, 5], [95, 95], [65, 95], [55, 80]], "epsilon": 150000, "figure": { "edges": [[2, 5], [5, 4], [4, 1], [1, 0], [0, 8], [8, 3], [3, 7], [7, 11], [11, 13], [13, 12], [12, 18], [18, 19], [19, 14], [14, 15], [15, 17], [17, 16], [16, 10], [10, 6], [6, 2], [8, 12], [7, 9], [9, 3], [8, 9], [9, 12], [13, 9], [9, 11], [4, 8], [12, 14], [5, 10], [10, 15]], "vertices": [[20, 30], [20, 40], [30, 95], [40, 15], [40, 35], [40, 65], [40, 95], [45, 5], [45, 25], [50, 15], [50, 70], [55, 5], [55, 25], [60, 15], [60, 35], [60, 65], [60, 95], [70, 95], [80, 30], [80, 40]] }
 }
-const solution = {
+let solution = {
     "vertices": [
         [21, 28], [31, 28], [31, 87], [29, 41], [44, 43], [58, 70],
         [38, 79], [32, 31], [36, 50], [39, 40], [66, 77], [42, 29],
@@ -13,8 +13,9 @@ const solution = {
 
 function render(canvas, problem, solution) {
     const ctx = canvas.getContext('2d')
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#00000066"
-    ctx.fillRect(0, 0, 300, 300)
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // hole
     ctx.beginPath()
@@ -24,6 +25,7 @@ function render(canvas, problem, solution) {
     }
     ctx.fillStyle = "#e1ddd1"
     ctx.fill()
+
 
     // vertices
     ctx.beginPath()
@@ -39,6 +41,32 @@ function render(canvas, problem, solution) {
 }
 
 const canvas = document.getElementById("pose")
+const problemElem = document.getElementById("problem");
+const solutionElem = document.getElementById("solution");
+const renderProblemElem = document.getElementById("render_problem");
+const renderSolutionElem = document.getElementById("render_solution");
 
-render(canvas, problem, solution)
+problemElem.addEventListener('change', (event) => {
+    const reader = new FileReader();
+    reader.onload = ev => {
+        problem = JSON.parse(ev.target.result)
+    }
+    reader.readAsText(event.target.files[0]);
+});
+
+solutionElem.addEventListener('change', (event) => {
+    const reader = new FileReader();
+    reader.onload = ev => {
+        solution = JSON.parse(ev.target.result)
+    }
+    reader.readAsText(event.target.files[0]);
+});
+
+renderProblemElem.addEventListener('click', () => {
+    render(canvas, problem, { vertices: problem.figure.vertices });
+});
+
+renderSolutionElem.addEventListener('click', () => {
+    render(canvas, problem, solution)
+});
 
