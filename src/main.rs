8f296d16ff8ac4532a796f88292411e98491dee0
Yusuke Nojima <nojima@ynojima.com>
2021-07-09 22:05:21 +0900

    Translate InputJSON to Input

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,67 @@ struct FigureJSON {
     vertices: Vec<Vec<i64>>,
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Point {
+        Point { x, y }
+    }
+
+    fn squared_distance(&self, other: &Point) -> i64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        dx*dx + dy*dy
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Input {
+    hole: Vec<Point>,
+    figure: Figure,
+    epsilon: i64,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Edge {
+    v: usize,
+    w: usize,
+}
+
+impl Edge {
+    fn new(v: usize, w: usize) -> Edge {
+        Edge { v, w }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Figure {
+    edges: Vec<Edge>,
+    vertices: Vec<Point>,
+}
+
 fn main() {
     let mut data = String::new();
     std::io::stdin().read_to_string(&mut data).unwrap();
 
-    let input: InputJSON = serde_json::from_str(&data).expect("failed to parse input as JSON");
+    let input_json: InputJSON = serde_json::from_str(&data).expect("failed to parse input as JSON");
+
+    let hole: Vec<Point> = input_json.hole.iter().map(|p| Point::new(p[0], p[1])).collect();
+    let edges: Vec<Edge> = input_json.figure.edges.iter().map(|e| Edge::new(e[0], e[1])).collect();
+    let vertices: Vec<Point> = input_json.figure.vertices.iter().map(|p| Point::new(p[0], p[1])).collect();
+
+    let input = Input{
+        hole,
+        figure: Figure{
+            edges,
+            vertices,
+        },
+        epsilon: input_json.epsilon,
+    };
 
     println!("input = {:?}", input);
 }
