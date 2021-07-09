22b1d59b7d4831430de890952d22f55dd310ccbb
Yusuke Nojima <nojima@ynojima.com>
2021-07-09 21:52:55 +0900

    Read input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::Read;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct InputJSON {
+    hole: Vec<Vec<i64>>,
+    figure: FigureJSON,
+    epsilon: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FigureJSON {
+    edges: Vec<Vec<usize>>,
+    vertices: Vec<Vec<i64>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut data = String::new();
+    std::io::stdin().read_to_string(&mut data).unwrap();
+
+    let input: InputJSON = serde_json::from_str(&data).expect("failed to parse input as JSON");
+
+    println!("input = {:?}", input);
 }
