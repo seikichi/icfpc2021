2a3a9b78597baa79744448e8b9aa7bd6bb8a5f74
Yusuke Nojima <nojima@ynojima.com>
2021-07-10 17:15:48 +0900

    Longer timeout

--- solver/src/main.rs
+++ solver/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let input = read_input();
     if let Some((solution1, dislike1)) = solvers::dfs::solve(&input) {
         eprintln!("dfs: dislike = {}", dislike1);
-        let (solution2, dislike2) = solvers::hill_climbing::solve(&input, solution1, Duration::from_millis(2000));
+        let (solution2, dislike2) = solvers::hill_climbing::solve(&input, solution1, Duration::from_millis(10000));
         let j = vertices_to_pose_json(&solution2);
         println!("{}", j);
         if !common::does_valid_pose(&solution2, &input.figure, &input.hole, input.epsilon) {
