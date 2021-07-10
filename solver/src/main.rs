98c456db418736e6a92805b8ac69b01344a9e13f
Yusuke Nojima <nojima@ynojima.com>
2021-07-10 19:43:44 +0900

    add SKIP_ORTHO

--- solver/src/main.rs
+++ solver/src/main.rs
@@ -8,6 +8,7 @@ use std::time::Duration;
 fn main() {
     let disable_dfs_centroid = std::env::var("DISABLE_DFS_CENTROID").is_ok();
     let use_hill_climbing = std::env::var("USE_HILL_CLIMBING").is_ok();
+    let skip_ortho = std::env::var("SKIP_ORTHO").is_ok();
     let hill_climbing_time_limit = {
         if let Ok(s) = std::env::var("HILL_CLIMBING_TIME_LIMIT_SECONDS") {
             let f: f64 = s.parse().expect("Invalid HILL_CLIMBING_TIME_LIMIT_SECONDS");
@@ -22,11 +23,16 @@ fn main() {
     if let Some((solution1, dislike1)) = solvers::dfs::solve(&input, disable_dfs_centroid) {
         eprintln!("dfs: dislike = {}", dislike1);
 
-        // orthgonal1
-        let mut input2 = input.clone();
-        input2.figure.vertices = solution1;
-        let (solution2, dislike2) = solvers::orthgonal::solve(&input2).unwrap();
-        eprintln!("orthgonal: dislike = {}", dislike2);
+        let solution2 = if skip_ortho {
+            solution1
+        } else {
+            // orthgonal1
+            let mut input2 = input.clone();
+            input2.figure.vertices = solution1;
+            let (solution2, dislike2) = solvers::orthgonal::solve(&input2).unwrap();
+            eprintln!("orthgonal: dislike = {}", dislike2);
+            solution2
+        };
 
         let (solution3, dislike3) = if use_hill_climbing {
             eprintln!("hill climbing...");
@@ -37,11 +43,16 @@ fn main() {
         };
         eprintln!("hill_climbing/annealing: dislike = {}", dislike3);
 
-        // orthgonal2
-        let mut input3 = input.clone();
-        input3.figure.vertices = solution3;
-        let (solution4, dislike4) = solvers::orthgonal::solve(&input3).unwrap();
-        eprintln!("orthgonal: dislike = {}", dislike4);
+        let solution4 = if skip_ortho {
+            solution3
+        } else {
+            // orthgonal2
+            let mut input3 = input.clone();
+            input3.figure.vertices = solution3;
+            let (solution4, dislike4) = solvers::orthgonal::solve(&input3).unwrap();
+            eprintln!("orthgonal: dislike = {}", dislike4);
+            solution4
+        };
 
         // adjust
         let (solution5, dislike5) = solvers::adjust::solve(&input, solution4);
