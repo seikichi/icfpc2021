06c5463f83f8c16a5a77b0b2d2ae4030e5b2f4c3
Seikichi KONDO <seikichi@kmc.gr.jp>
2021-07-10 10:40:38 +0900

    Add material UI in www

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,6 +1,7 @@
 import { promises as fs } from 'fs'
 import path from "path";
 import Link from 'next/link'
+import { Button, ButtonGroup } from '@material-ui/core'
 
 interface Props {
   ids: readonly string[];
@@ -19,6 +20,20 @@ export async function getStaticProps() {
 export default function Home({ ids }: Props) {
   return (
     <>
+      <div style={{ margin: '0.5em' }}>
+        <Button variant="contained">Default</Button>{' '}
+        <Button variant="contained" color="primary">Primary</Button>{' '}
+        <Button variant="contained" color="secondary">Secondary</Button>{' '}
+        <Button variant="contained" disabled>Disabled</Button>{' '}
+        <Button variant="contained" color="primary" href="https://google.com/">LINK</Button>
+      </div>
+      <div style={{ margin: '0.5em' }}>
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+      </div>
       <h3>Solutions</h3>
       <ul>
         {
