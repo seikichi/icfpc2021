5edf6ba13f0cf2fab3da2bbe3a97a734c9534e85
MORI Shingo <cos@kmc.gr.jp>
2021-07-11 21:25:33 +0900

    add shrink solver

--- solver/src/solvers/mod.rs
+++ solver/src/solvers/mod.rs
@@ -1,5 +1,6 @@
-pub mod dfs;
-pub mod orthgonal;
-pub mod hill_climbing;
-pub mod annealing;
 pub mod adjust;
+pub mod annealing;
+pub mod dfs;
+pub mod hill_climbing;
+pub mod orthgonal;
+pub mod shrink;
--- solver/src/solvers/orthgonal.rs
+++ solver/src/solvers/orthgonal.rs
@@ -24,6 +24,25 @@ fn mirror_x_in_place(figure: &mut Figure) {
     }
 }
 
+fn calc_bound_box(ps: &Vec<Point>) -> (Point, Point) {
+    let mut ret = (Point::new(1e+9, 1e+9), Point::new(-1e+9, -1e+9));
+    for &p in ps.iter() {
+        if p.x() < ret.0.x() {
+            ret.0.set_x(p.x());
+        }
+        if p.y() < ret.0.y() {
+            ret.0.set_y(p.y());
+        }
+        if ret.1.x() < p.x() {
+            ret.1.set_x(p.x());
+        }
+        if ret.1.y() < p.y() {
+            ret.1.set_y(p.y());
+        }
+    }
+    return ret;
+}
+
 fn try_all_translations(
     original_figure: &Figure,
     hole: &Polygon,
@@ -32,8 +51,20 @@ fn try_all_translations(
     let mut figure = original_figure.clone();
     let mut best_vertices = None;
     let mut best_dislike = best_dislike;
-    for dy in -100..=100 {
-        for dx in -100..=100 {
+
+    let hole_points: Vec<Point> = hole.exterior().points_iter().skip(1).collect();
+    let hole_bound_box = calc_bound_box(&hole_points);
+    let figure_bound_box = calc_bound_box(&figure.vertices);
+    let l = (hole_bound_box.0.x() - figure_bound_box.0.x()) as i64;
+    let r = (hole_bound_box.1.x() - figure_bound_box.1.x()) as i64;
+    let u = (hole_bound_box.0.y() - figure_bound_box.0.y()) as i64;
+    let b = (hole_bound_box.1.y() - figure_bound_box.1.y()) as i64;
+    // eprintln!("hole bound box: {:?}", hole_bound_box);
+    // eprintln!("figure bound box: {:?}", figure_bound_box);
+    // eprintln!("lrub: {} {} {} {}", l, r, u, b);
+
+    for dy in l..=r {
+        for dx in u..=b {
             translate(original_figure, dx as f64, dy as f64, &mut figure);
             let dislike = calculate_dislike(&figure.vertices, hole);
             if dislike >= best_dislike {
