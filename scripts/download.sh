071685d5672ad60c8016746edebf9cb8aadd7579
MORI Shingo <cos@kmc.gr.jp>
2021-07-11 09:26:50 +0900

    add problems

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(git rev-parse --show-toplevel)"
-for i in $(seq 1 88)
+for i in $(seq 1 106)
 do
   echo ${i}
   curl "https://poses.live/problems/${i}/download" > "problems/${i}.problem"
